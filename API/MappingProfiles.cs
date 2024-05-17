c21f3c8b2ca77c9406d4045d4251e042b4327133
ajibade-ibrahim <ibrahim.ajibade@hotmail.com.com>
2024-05-17 20:51:01 +0100

    Added mapping profile for automapper

---
+++ API/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿using API.Models.Dto;
+using AutoMapper;
+
+namespace API
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Models.Program, CreateProgramDto>().ReverseMap();
+            CreateMap<Models.Program,ProgramDto>().ReverseMap();
+            CreateMap<Models.Question,CreateQuestionDto>().ReverseMap();
+            CreateMap<Models.Question,QuestionDto>().ReverseMap();
+            CreateMap<Models.Application,CreateApplicationDto>().ReverseMap();
+            CreateMap<Models.Answer,AnswerDto>().ReverseMap();
+        }
+    }
+}
