9322bd1760fc120ed25f1828c6ae6b2d2e18d328
ajibade-ibrahim <ibrahim.ajibade@hotmail.com.com>
2024-05-17 20:51:31 +0100

    Added configurations and dependendy injections

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,7 @@
+using API.Data;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var config = new ConfigurationBuilder()
+  .AddJsonFile("appsettings.json")
+  .Build();
+var localConnectionString = config["LocalCosmosConnectionString"];
+
+builder.Services.AddDbContextFactory<ProgramContext>(optionsBuilder =>
+  optionsBuilder
+    .UseCosmos(
+      connectionString: localConnectionString,
+      databaseName: "ProgramsDb",
+      cosmosOptionsAction: options =>
+      {
+        options.ConnectionMode(Microsoft.Azure.Cosmos.ConnectionMode.Gateway);
+      }));
+builder.Services.AddTransient<ProgramsService>();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -15,7 +37,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(x => x
+              .AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
